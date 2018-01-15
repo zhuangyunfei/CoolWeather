46a0c730fc4676cb1921ea16fb787ec3db746e74
zhuangyunfei <33480828+zhuangyunfei@users.noreply.github.com>
2018-01-15 13:13:28 +0800

    数据库、表配置
    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,49 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 项目名称
+ * 创建人
+ * 创建时间
+ * 说明
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
